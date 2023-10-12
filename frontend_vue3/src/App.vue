7a0c875a26861e91c4f20c1f3b32b88ba7e56d2b
Gujer Nicolas Joël <nicolas.gujer@bfh.ch>
2023-10-12 17:59:12 +0200

    first commit

---
+++ frontend_vue3/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MainPage from '@/views/MainPage.vue'
+
+</script>
+
+<template>
+  <Suspense>
+    <MainPage></MainPage>
+  </Suspense>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend_vue3/src/views/MainPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const data = ref("")
+const request = await fetch("http://127.0.0.1:8000")
+data.value = await request.text()
+</script>
+
+<template>
+    <h1>hello world</h1>
+    <p>{{ data }}</p>
+</template>
