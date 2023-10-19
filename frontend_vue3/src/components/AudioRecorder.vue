d406bb200621c63e1e01987559c40b5343c4619b
AngryBacteria <AngryBacteria@protonmail.com>
2023-10-19 20:08:34 +0200

    better recorder setup

--- frontend_vue3/src/components/AudioRecorder.vue
+++ frontend_vue3/src/components/AudioRecorder.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
 import { RecordRTCPromisesHandler, StereoAudioRecorder, invokeSaveAsDialog } from 'recordrtc'
 
-async function startRecording() {
-  let stream = await navigator.mediaDevices.getUserMedia({ audio: true })
-  let recorder = new RecordRTCPromisesHandler(stream, {
+let stream: MediaStream
+let recorder: RecordRTCPromisesHandler
+
+async function initRecorder() {
+  stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+  recorder = new RecordRTCPromisesHandler(stream, {
     recorderType: StereoAudioRecorder,
     mimeType: 'audio/wav',
     disableLogs: false,
+    //no benefit for speech to text, but more = better normally
     numberOfAudioChannels: 1,
-    sampleRate: 44100,
+    //leave this
     desiredSampRate: 16000,
+    //Higher = better quality but higher file size
+    bufferSize: 4096,
+    //How many milliseconds until new data gets sent
+    timeSlice: 1500,
     ondataavailable: function (blob) {
       console.log('SOME NEW DATA', blob)
     }
   })
-  recorder.startRecording()
+}
 
-  const sleep = (m: number | undefined) => new Promise((r) => setTimeout(r, m))
-  await sleep(3000)
+async function startRecording() {
+  await initRecorder()
+  await recorder.startRecording()
+}
 
+async function stopRecording() {
   await recorder.stopRecording()
   let blob = await recorder.getBlob()
   invokeSaveAsDialog(blob)
+  await recorder.destroy()
 }
 </script>
 
 <template>
   <h2>This is the audio recorder component</h2>
   <button @click="startRecording()">Start Audio Recording</button>
+  <button @click="stopRecording()">Stop and Save</button>
 </template>
