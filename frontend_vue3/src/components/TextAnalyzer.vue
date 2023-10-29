2ae2543c6deed8d0922f46fb34ce4bbd8f1e87a4
Jorma Steiner <jorma.steiner@students.bfh.ch>
2023-10-29 18:40:08 +0100

    small changes & prompting simulated info exchange

--- frontend_vue3/src/components/TextAnalyzer.vue
+++ frontend_vue3/src/components/TextAnalyzer.vue
@@ -2,7 +2,7 @@
   <section class="component-wrapper">
     <h3>Füge den transkribierten Text hier ein</h3>
     <Textarea v-model="transcription" autoResize rows="4" />
-    <Button @click="analyzeText()">Analyze Text</Button>
+    <Button @click="analyzeText()" :disabled="(transcription.length < 1) || analysisIsLoading">Analysiere Text</Button>
 
     <Accordion>
       <AccordionTab header="Extrahierte Informationen">
@@ -21,19 +21,42 @@ const transcription = ref('');
 const extractedInfo = ref('');
 const analysisIsLoading = ref(false);
 const userStore = useUserStore();
-let llmApiUrl = 'http://127.0.0.1:8000/api/llm/openai/gpt-3.5-turbo';
+let llmApiUrl = 'http://localhost:8000/api/llm/openai/gpt-3.5-turbo';
 
 async function analyzeText() {
   if (!transcription.value.trim()) return; // No Input no output.
 
   analysisIsLoading.value = true;
 
+  let prev_messages = [
+    {
+      role: 'system',
+      content: 'Du bist ein System, das aus einem Transkript eines gesprochenen Dialogs in einem medizinischen Kontext die Symptome und Medikamente des Patienten extrahiert und in einem JSON-Format zurückgibt.'
+    },
+    {
+      role: 'user',
+      content: 'Das ist Franz Feldmann, 66 Jahre. Hat vernichtende Brustschmerzen seit etwa 30 Minuten. Strahlen aus in den linken Arm. Hat etwas Dyspnö, ist nicht synkopiert. Keine KHK bekannt. Hat noch Hypertonus, kennt aber seine Medis nicht. Vitalparameter waren Blutdruck 145 zu 90, Puls 95, Sauerstoffsättigung 93% an Raumluft. Haben mal 4 Liter Sauerstoff und 2mg Morphin gegeben. Das EKG hatten wir Euch schon per Mail geschickt. Da hat es Senkungen in V2 bis V4. Wir haben mit Heparin noch gewartet, weil er nicht sicher war, ob er auch was zur Blutverdünnung nimmt. Ich nehme Tabletten für meinen Bluthochdruck, aber den Namen weiß ich nicht.'
+    },
+    {
+      role: 'system',
+      content: JSON.stringify({
+        "Symptome": ["halbstündige Brustschmerzen", "leichte Dyspnö", "Hypertonus"],
+        "Medikamente": ["Bluthochdruckmedikamente"]
+      })
+    }
+  ]
+
   //Load prompt and insert transcript
   let prompt = userStore.openAiPrompt.replace('<PLACEHOLDER>', transcription.value);
 
+  //TODO handling too large file
   //TODO relocate max tokens into backend
+  
+  let { temperature, presence_penalty, top_p, frequency_penalty } = userStore.openAiConfig;
+
   let requestBody = {
     messages: [
+      ...prev_messages,
       {
         role: 'user',
         content: prompt
@@ -41,10 +64,10 @@ async function analyzeText() {
     ],
     config: {
       max_tokens: 50,
-      temperature: userStore.openAiConfig.temperature,
-      presence_penalty: userStore.openAiConfig.presence_penalty,
-      top_p: userStore.openAiConfig.top_p,
-      frequency_penalty: userStore.openAiConfig.frequency_penalty
+      temperature,
+      presence_penalty,
+      top_p,
+      frequency_penalty,
     }
   };
 
@@ -60,8 +83,12 @@ async function analyzeText() {
     if (answer.length > 1) {
       extractedInfo.value = answer;
     }
+    else {
+      extractedInfo.value = "Keine Informationen konnten herausgezogen werden."
+    }
   } catch (error) {
     console.error('Error:', error);
+    extractedInfo.value = "Während dem Analysieren geschah ein Fehler. Versuche erneut."
   } finally {
     analysisIsLoading.value = false;
   }
--- frontend_vue3/src/stores/user.ts
+++ frontend_vue3/src/stores/user.ts
@@ -32,16 +32,9 @@ export const useUserStore = defineStore('user', () => {
   const openAiPrompt = useLocalStorage(
     'openAiPrompt',
     `
-  Gegeben ist das folgende Transkript eines Dialogs zwischen Ärzten und Patienten. Bitte extrahiere spezifische Informationen über Symptome und Medikamente und gib diese im JSON-Format zurück:
+    Bitte extrahiere spezifische Informationen über Symptome und Medikamente aus diesem Transkript:
 
-  <PLACEHOLDER>
-
-  Bitte geben Sie das Ergebnis im folgenden Format:
-
-  {
-  "Symptome": ["Symptom1", "Symptom2", ...],
-  "Medikamente": ["Medikament1", "Medikament2", ...]
-  }
+    <PLACEHOLDER>    
   `
   );
 
