2291616be992edb80634b68e0bcddd73303e099f
AngryBacteria <AngryBacteria@protonmail.com>
2023-12-19 19:38:18 +0100

    replaced csv with pk1 files

--- backend_fastapi/app/utils/embedding_util.py
+++ backend_fastapi/app/utils/embedding_util.py
@@ -1,20 +1,14 @@
-import ast
 import os
 
+import numpy as np
+import pandas as pd
 from dotenv import load_dotenv
 from numpy import dot
 from numpy.linalg import norm
 from pandas import DataFrame
-import numpy as np
-import pandas as pd
 
 from app.utils.logging_util import logger
 from app.utils.openai_util import OpenAIUtil
-
-
-def convert_to_array(embedding_str):
-    """Used to convert a string from the csv back into a np array"""
-    return np.array(ast.literal_eval(embedding_str))
 
 
 def get_cosine_similarity(a, b):
@@ -29,9 +23,9 @@ class EmbeddingUtil(object):
     icd10_symptoms: DataFrame
     embedding_column: str = "openai_embedding"
     openai_util: OpenAIUtil = None
-    # path to the icd-10 csv files. Can be set or left blank
-    csv_folder_path: str
-    csv_file_name: str
+    # path to the icd-10 PKL files. Can be set or left blank
+    data_folder_path: str
+    data_file_name: str
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
@@ -40,8 +34,8 @@ class EmbeddingUtil(object):
 
     def __init__(
         self,
-        csv_folder_path: str = "app/data/catalogs/",
-        csv_file_name: str = "icd10gm_symptoms.csv",
+        data_folder_path: str = "app/data/catalogs/",
+        data_file_name: str = "icd10gm_symptoms.pkl",
     ):
         if hasattr(self, "_initialized"):
             return
@@ -51,38 +45,33 @@ class EmbeddingUtil(object):
             raise EnvironmentError(".env file is missing the OPENAI_API_KEY")
 
         # check if folder/file paths exist
-        self.csv_folder_path = csv_folder_path
-        self.csv_file_name = csv_file_name
-        if not os.path.exists(os.path.join(self.csv_folder_path)):
-            raise EnvironmentError(f"CSV path does not exist: {self.csv_folder_path}")
-        icd10_file_path = os.path.join(self.csv_folder_path, self.csv_file_name)
+        self.data_folder_path = data_folder_path
+        self.data_file_name = data_file_name
+        if not os.path.exists(os.path.join(self.data_folder_path)):
+            raise EnvironmentError(f"PKL path does not exist: {self.data_folder_path}")
+        icd10_file_path = os.path.join(self.data_folder_path, self.data_file_name)
         if not os.path.exists(icd10_file_path):
             raise EnvironmentError(
-                f"ICD-10 CSV File does not exist at path: {icd10_file_path}"
+                f"ICD-10 PKL File does not exist at path: {icd10_file_path}"
             )
 
         self.openai_util = OpenAIUtil()
-        self.icd10_symptoms = pd.read_csv(
-            os.path.join(self.csv_folder_path, self.csv_file_name), sep=","
+        self.icd10_symptoms = pd.read_pickle(
+            os.path.join(self.data_folder_path, self.data_file_name)
         )
-        # if the embeddings exist already, convert them back into an ndArray
-        if self.embedding_column in self.icd10_symptoms.columns:
-            self.icd10_symptoms[self.embedding_column] = self.icd10_symptoms[
-                self.embedding_column
-            ].apply(convert_to_array)
-        else:
+        # check if the embeddings exist already
+        if self.embedding_column not in self.icd10_symptoms.columns:
             logger.warning("icd10 file has no embeddings, the search wont work")
 
         logger.info("Created EmbeddingUtil")
 
     def create_icd10_symptoms_embeddings(self):
-        """Creates the embeddings for the symptoms (category R) and saves them into the original csv"""
+        """Creates the embeddings for the symptoms (category R) and saves them into the original PKL"""
         self.icd10_symptoms[self.embedding_column] = self.icd10_symptoms[
             "klassentitel"
         ].apply(lambda x: self.openai_util.get_embedding(x))
-        self.icd10_symptoms.to_csv(
-            os.path.join(self.csv_folder_path, self.csv_file_name),
-            index=False,
+        self.icd10_symptoms.to_pickle(
+            os.path.join(self.data_folder_path, self.data_file_name),
         )
 
     def search(self, df: DataFrame, text: str, n=10):
